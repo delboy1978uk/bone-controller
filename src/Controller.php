d39b52a84555db618e0d7134ffc89da050bd89fd
Derek Stephen McLean <delboy1978uk@gmail.com>
2020-11-26 19:40:06 +0100

    send response with layout header

--- src/Controller.php
+++ src/Controller.php
@@ -8,10 +8,20 @@ use Bone\I18n\Traits\HasTranslatorTrait;
 use Bone\View\Traits\HasViewTrait;
 use Bone\View\ViewAwareInterface;
 use Bone\Server\SiteConfigAwareInterface;
+use Psr\Http\Message\ResponseInterface;
 
 class Controller implements I18nAwareInterface, ViewAwareInterface, SiteConfigAwareInterface
 {
     use HasSiteConfigTrait;
     use HasTranslatorTrait;
     use HasViewTrait;
+
+    /**
+     * @param ResponseInterface $response
+     * @param string $layout
+     */
+    public function responseWithLayout(ResponseInterface $response, string $layout)
+    {
+        return $response->withHeader('layout', $layout);
+    }
 }
